69abef2d7dc20fb5f268f6168524c3a2081a3a15
mana-sys <chtka.mana@gmail.com>
2020-01-14 15:05:47 -0800

    Set up provider variables.

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  region = var.aws_region
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket_prefix = "rmd-tf-starter"
 }
---
+++ environments/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key_id" {
+  description = "The AWS access key to use for API operations."
+}
+
+variable "aws_secret_access_key" {
+  description = "The AWS secret key to use for API operations."
+}
+
+variable "aws_region" {
+  description = "The AWS region where operations will take place."
+}
