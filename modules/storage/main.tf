65c38cf2df83129c06ae275fa2cdb950e131a8cc
mana-sys <chtka.mana@gmail.com>
2020-01-14 17:51:21 -0800

    Add AES256 encryption by default.

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "bucket" {
   bucket_prefix = var.bucket_prefix
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
